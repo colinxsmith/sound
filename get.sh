dfa1479acb72bc637712f629007e8be38eab4b39
Colin <colinxsmith@gmail.com>
2018-11-26 23:23:27 +0000

    Get it right

--- get.sh
+++ get.sh
@@ -4,6 +4,6 @@ rm ~/Music/progMon.aac
 cat ~/Music/playlists/stations.m3u
 for i in `sed -n "/radio3/p" ~/Music/playlists/stations.m3u`
 do
-avconv -i $i  -codec: copy -t $((1.5*3600)) ~/Music/progMon.aac
+avconv -i $i  -codec: copy -t $((15*360)) ~/Music/progMon.aac
 exit
 done
--- musicproc.sh
+++ musicproc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in $(cat streams.txt); do curl -q $i |sed "s/File[0-9]=//"| sed -n "/http/p" ;done > ~/Music/playlists/stations.m3u
+for i in $(cat ~/sound/streams.txt); do curl -q $i |sed "s/File[0-9]=//"| sed -n "/http/p" ;done > ~/Music/playlists/stations.m3u
