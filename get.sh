3b31d5ba0364032bb3f9c936108d73b2b4c92d10
Colin Smith <colin.smith@corfinancialgroup.com>
2018-11-27 15:27:24 +0000

    Do them all

--- get.sh
+++ get.sh
@@ -3,7 +3,7 @@
 rm ~/Music/progMon*.aac
 cat ~/Music/playlists/stations.m3u
 j=0
-for i in `sed -n "/radio3/p" ~/Music/playlists/stations.m3u`
+for i in `cat ~/Music/playlists/stations.m3u`
 do
 j=$((j+1))
 avconv -i $i  -codec: copy -t $((15*360)) ~/Music/progMon$j.aac
