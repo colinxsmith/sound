b750827b8dd985d30b59b780a913952542bddee8
Colin <colinxsmith@gmail.com>
2018-11-28 23:29:37 +0000

    add example kkk

--- musicproc.sh
+++ musicproc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in $(cat ~/sound/streams.txt); do curl -q $i |sed "s/File[0-9]=//"| sed -n "/http/p" ;done > ~/Music/playlists/stations.m3u
+for i in $(cat ~/sound/streams.txt); do curl -q $i |sed "/File1/d;s/File[0-9]=//"| sed -n "/http/p" ;done > ~/Music/playlists/stations.m3u
