03127ef43164f26b59ebbb2ef03d10a64fa0aa1c
Colin Smith <colinxsmith@gmail.com>
2019-12-21 22:47:39 +0000

    Update youGet.sh

--- youGet.sh
+++ youGet.sh
@@ -7,7 +7,7 @@ cd
 
 #youtube-dl -x --audio-format $fmt https://www.youtube.com/playlist?list=OLAK5uy_lcT87h1eIuzRBRkZZZ-lil10-Xt7uood4
 echo youtube-dl -x --audio-format $fmt "$1" -v --prefer-ffmpeg
-youtube-dl -x --audio-format $fmt "$1" -v --prefer-ffmpeg
+youtube-dl -x --audio-format $fmt "$1" -v --fixup --prefer-ffmpeg
 
 echo youtube-dl -x --audio-format $fmt "$2" -v --prefer-ffmpeg
 youtube-dl -x --audio-format $fmt "$2" -v --prefer-ffmpeg
