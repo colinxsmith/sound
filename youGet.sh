a7ffa6180e93d9ffbf86794f3e58fb5c26a38b9e
Colin <colinxsmith@gmail.com>
2019-12-21 18:44:26 +0000

    fmt

--- youGet.sh
+++ youGet.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 PATH=$PATH:/usr/local/bin
+fmt=m4a
 echo $HOME
 cd
 
 
-#youtube-dl -x --audio-format m4a https://www.youtube.com/playlist?list=OLAK5uy_lcT87h1eIuzRBRkZZZ-lil10-Xt7uood4
-echo youtube-dl -x --audio-format m4a "$1"
-youtube-dl -x --audio-format m4a "$1"
+#youtube-dl -x --audio-format $fmt https://www.youtube.com/playlist?list=OLAK5uy_lcT87h1eIuzRBRkZZZ-lil10-Xt7uood4
+echo youtube-dl -x --audio-format $fmt "$1"
+youtube-dl -x --audio-format $fmt "$1"
 
-echo youtube-dl -x --audio-format m4a "$2"
-youtube-dl -x --audio-format m4a "$2"
+echo youtube-dl -x --audio-format $fmt "$2"
+youtube-dl -x --audio-format $fmt "$2"
 
-echo youtube-dl -x --audio-format m4a "$3"
-youtube-dl -x --audio-format m4a "$3"
+echo youtube-dl -x --audio-format $fmt "$3"
+youtube-dl -x --audio-format $fmt "$3"
 
-for i in *.m4a
+for i in *.$fmt
 do
-echo cp "$i" $HOME/Music/$(echo $i | sed "s/ /_/g;s/-[A-Z,a-z,0-9].*.m4a/.m4a/;s/ft\./ft/")
-cp "$i" $HOME/Music/$(echo $i | sed "s/ /_/g;s/-[A-Z,a-z,0-9].*.m4a/.m4a/;s/ft\./ft/")
+echo cp "$i" $HOME/Music/$(echo $i | sed "s/ /_/g;s/-[A-Z,a-z,0-9].*.$fmt/.$fmt/;s/ft\./ft/")
+cp "$i" $HOME/Music/$(echo $i | sed "s/ /_/g;s/-[A-Z,a-z,0-9].*.$fmt/.$fmt/;s/ft\./ft/")
 done
 
-echo rm $HOME/*.m4a
-rm $HOME/*.m4a
+echo rm $HOME/*.$fmt
+rm $HOME/*.$fmt
