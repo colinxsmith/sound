244ba8f72bc0dd4a1db9461f290d29efe440d539
Colin Smith <colinxsmith@gmail.com>
2020-03-25 23:22:11 +0000

    Update get.sh

--- get.sh
+++ get.sh
@@ -23,8 +23,8 @@ j=0
 for i in `sed -n "/$station/p" ~/Music/playlists/*.m3u`
 do
 j=$((j+1))
-echo ffmpeg -i $i   -t $time ~/Music/prog$Day$j.$end
-ffmpeg -i $i   -t $time ~/Music/prog$Day$j.$end
+echo ffmpeg -i $i -t $time - ab 320k ~/Music/prog$Day$j.$end
+ffmpeg -i $i   -t $time -ab 320k ~/Music/prog$Day$j.$end
 if [ $j -eq 1 ]
 then
 	break
