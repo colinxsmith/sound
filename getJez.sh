06e1eb5f79dcbe1825df7fec084e8ebcd7ebc80f
colin <colin@HPTeddy.HPTeddy-domain>
2020-11-03 20:58:14 +0000

    get Somethin Else

---
+++ getJez.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+cd ~/Music
+curl $(curl https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
+rm -rf jez.mp3
+ffmpeg -ss 7200 -i j3hour.mp3 jez.mp3
