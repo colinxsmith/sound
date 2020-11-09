c272c7c75a3e93cbe0fcc3085aae7c017cdf750d
Colin <colinxsmith@gmail.com>
2020-11-09 17:05:51 +0000

    .

--- getJez.sh
+++ getJez.sh
@@ -7,5 +7,9 @@ do
   sleep 3600
   curl $(curl https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
 done
-rm -rf jez.mp3
-ffmpeg -ss 7200 -i j3hour.mp3 jez.mp3
+if [ $(ls -l j3hour.mp3|awk '{print $5;}') != 0 ]
+then
+  echo update jez.mp3
+  rm -rf jez.mp3
+  ffmpeg -ss 7200 -i j3hour.mp3 jez.mp3
+fi
