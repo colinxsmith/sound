2b5af4895092b23bb6266ee951b5265140443e3e
Colin <colinxsmith@gmail.com>
2020-11-10 11:18:44 +0000

    .

--- getJez.sh
+++ getJez.sh
@@ -7,8 +7,8 @@ do
   echo sleeping
   sleep 3600
   curl $(curl https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
+  ls -l j3hour.mp3
 done
-ls -l j3hour.mp3
 if [ $(ls -l j3hour.mp3|awk '{print $5;}') -gt 10 ]
 then
   echo update jez.mp3
