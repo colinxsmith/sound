ee5d616f2b075a8abaab7a6d47b05178506a2f1d
Colin <colinxsmith@gmail.com>
2020-11-10 11:13:24 +0000

    change the file test

--- getJez.sh
+++ getJez.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 cd ~/Music
 curl $(curl https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
-while [ $(ls -l j3hour.mp3|awk '{print $5;}') = 0 ]
+ls -l j3hour.mp3
+while [ $(ls -l j3hour.mp3|awk '{print $5;}') -lt 10 ]
 do
   echo sleeping
   sleep 3600
   curl $(curl https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
 done
-if [ $(ls -l j3hour.mp3|awk '{print $5;}') != 0 ]
+ls -l j3hour.mp3
+if [ $(ls -l j3hour.mp3|awk '{print $5;}') -gt 10 ]
 then
   echo update jez.mp3
   rm -rf jez.mp3
