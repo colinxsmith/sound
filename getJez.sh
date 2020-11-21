61f1b8400a0292aa04744577bc0a772b6ca2efbf
colin <colin@HPTeddy.HPTeddy-domain>
2020-11-21 22:58:43 +0000

    More clever?

--- getJez.sh
+++ getJez.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 cd ~/Music
+stamp=$(date +%m%dT01)
+#stamp=1116T01
+all=$(curl -L https://planetradio.co.uk/jazz-fm/shows/somethin-else-with-jez-nelson/ |  sed -n "s|https://downl|\nhttps://downl|gp"|sed -n "/^http/p"|sed "s/mp3.*/mp3/")
+latest=no
+for i in $all
+do
+  latest=$i
+  break
+done
+found=$(echo $latest|sed -n "/$stamp/p")
+echo $found
 curl $(curl -L https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
 ls -l j3hour.mp3
+if [ $(ls -l j3hour.mp3|awk '{print $5;}') -lt 10 ]
+then
+  curl $found > j3hour.mp3
+fi
 while [ $(ls -l j3hour.mp3|awk '{print $5;}') -lt 10 ]
 do
   echo sleeping
