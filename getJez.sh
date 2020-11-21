d7ba7b0deaeccb2523500f5a6944375d411683f8
Colin Smith <colinxsmith@gmail.com>
2020-11-21 20:28:31 +0000

    Update getJez.sh

--- getJez.sh
+++ getJez.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/Music
-curl $(curl https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
+curl $(curl -L https://planetradio.co.uk/jazz-fm/schedule/ | sed -n "s|https://down|\nhttps://down|gp" | sed -n "/somethin_else.*3\":\"$/s/mp3.*/mp3/p") > j3hour.mp3
 ls -l j3hour.mp3
 while [ $(ls -l j3hour.mp3|awk '{print $5;}') -lt 10 ]
 do
