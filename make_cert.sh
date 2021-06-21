d00c08dc40e7b598367faeee15c1b5a7c5108ced
Colin <colinxsmith@gmail.com>
2021-06-21 11:44:35 +0100

    Cert file

---
+++ make_cert.sh
@@ -0,0 +1,2 @@
+ openssl req -newkey rsa:2048 -x509 -keyout bita-dev-key.pem -out bita-dev-cert.pem -days 3650
+ openssl pkcs12 -export -inkey bita-dev-key.pem -in bita-dev-cert.pem -out bita-dev.pkcs12 -name 'Big Test'
