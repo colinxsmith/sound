19582d9ee4342a272f596ed0a0d8cad769e42eeb
Colin Smith <colinxsmith@gmail.com>
2024-09-23 17:22:18 +0100

    .

--- reverse_names.sh
+++ reverse_names.sh
@@ -1,5 +1,1 @@
- ls Jeff_Beck*.mp3 | tr ' ' '\n'|tac|tr '\n' ' ' > aa
- cat aa
- for i in $(cat aa); do mpc insert $i; done
- mpc playlist |cat -n 
- 2022  mpc
+ for i in `ls Osi*.mp3|tac`; do mpc insert $i; done
