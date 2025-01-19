9c6fb7e09956d4cb2310cb8477b5e037368847c7
Colin Smith <colinxsmith@gmail.com>
2025-01-19 22:49:01 +0000

    use jazz.mp3

--- getjazz.sh
+++ getjazz.sh
@@ -6,4 +6,4 @@ TIME=${2:-2200}
 #both URLs work
 URL1=hellorayo
 URL2=planetradio
-curl -L la.$URL1.co.uk/jazz/Jazz-$DATE-$TIME.mp3 > j1.mp3
+curl -L la.$URL1.co.uk/jazz/Jazz-$DATE-$TIME.mp3 > jazz.mp3
