13f6b1850ef64df74898be49220bdbbcbd694426
Colin <colinxsmith@gmail.com>
2025-01-19 17:51:21 +0000

    More general script to get programs from Jazz FM

---
+++ getjazz.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Get any program from Jazz FM
+#DATE is the start time of the program
+DATE=${1:-20250112}
+TIME=${2:-2200}
+#both URLs work
+URL1=hellorayo
+URL2=planetradio
+curl -L la.$URL1.co.uk/jazz/Jazz-$DATE-$TIME.mp3 > j1.mp3
