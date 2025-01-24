d3b5fe76b74eaeb156faa0106240b31516371c41
Colin <colinxsmith@gmail.com>
2025-01-24 17:14:24 +0000

    new down load method

---
+++ newmeth.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo newmeth.sh \<start time eg. 2200 for 10pm\> \<day of week: 0 is Sunday 1 is Saturday \(one day before\)\>
+day=$(date +%w)
+today=$(date +%s)
+off=$(($(($((day + ${2:-0}))))*24*60*60))
+before=$(($today-$off))
+base=la.hellorayo.co.uk
+start=${1:-2200}
+secsoff=-3600
+
+echo start at time $start on $(date --date=@$(($before)) +%Y%m%d)
+
+file=https://$base/jazz/Jazz-$(date --date=@$(($before)) +%Y%m%d)-$start.mp3
+echo $file
+#curl -L $file > music.mp3
+
+if [ $start = '2200' ]
+then 
+    if [ ${2:-0} = '0' ]
+    then
+echo 10pm on Sunday
+curl -L $file > jn.mp3
+rm ~/Music/j3hour.mp3
+ln -s jn.mp3 ~/Music/j3hour.mp3
+    elif [ ${2:-0} = '1' ]
+    then
+echo 10pm on Saturday
+curl -L $file > ny.mp3
+rm ~/Music/j3hour.mp3
+ln -s ny.mp3 ~/Music/j3hour.mp3
+    fi
+elif [ $start = '1800' ]
+then
+    if [ ${2:-0} = '0' ]
+    then
+echo 6pm on Sunday
+    elif [ ${2:-0} = '1' ]
+    then
+echo 6pm on Saturday
+curl -L $file > cp.mp3
+rm ~/Music/j3hour.mp3
+ln -s cp.mp3 ~/Music/j3hour.mp3
+    fi
+elif [ $start = '1700' ]
+then
+    if [ ${2:-0} = '0' ]
+    then
+echo 5pm on Sunday
+curl -L $file > df.mp3
+rm ~/Music/j3hour.mp3
+ln -s df.mp3 ~/Music/j3hour.mp3
+    elif [ ${2:-0} = '1' ]
+    then
+echo 5pm on Saturday
+    fi
+fi
