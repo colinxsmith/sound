db403b68a23030903165af725a64564e2c80763a
Colin Smith <colinxsmith@gmail.com>
2025-04-05 18:20:46 +0100

    ...

--- getjazz.sh
+++ getjazz.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #Get any program from Jazz FM
 #DATE is the start time of the program
-DATE=${1:-20250118}
-TIME=${2:-1800}
+DATE=${1:-20250323}
+TIME=${2:-1700}
 #both URLs work
 URL1=hellorayo
 URL2=planetradio
-curl -L la.$URL1.co.uk/jazz/Jazz-$DATE-$TIME.mp3 > jazz.mp3
+curl -L la.$URL1.co.uk/jazz/Jazz-$DATE-$TIME.mp3 > df1.mp3
