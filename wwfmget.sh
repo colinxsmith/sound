102ba6a84406b609094fcd2261d5fbc785f05be1
Colin Smith <colinxsmith@gmail.com>
2025-05-02 22:14:40 +0100

    .

--- wwfmget.sh
+++ wwfmget.sh
@@ -3,4 +3,5 @@ if [ $(uname -n) != new64 ]; then export MPD_HOST=new64.local; fi
 uname -n
 echo $MPD_HOST
 echo $(mpc playlist | grep Worldw | awk -F: '{ print $2; }')
+cd
 sound/youGet.sh "$(mpc playlist | grep Worldw | awk -F: '{ print $2; }')"
