3b6e38ded8c2b9a02ce3efa17c815eb9b81da924
Colin Smith <colinxsmith@gmail.com>
2025-05-02 21:56:52 +0100

    Easy to record worldwide FM track

---
+++ wwfmget.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+if [ $(uname -n) != new64 ]; then export MPD_HOST=new64.local; fi
+uname -n
+echo $MPD_HOST
+echo $(mpc playlist | grep Worldw | awk -F: '{ print $2; }')
+sound/youGet.sh "$(mpc playlist | grep Worldw | awk -F: '{ print $2; }')"
