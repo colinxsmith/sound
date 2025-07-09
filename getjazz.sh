9ae2734beb69253cc83430afa006d0bded41546c
Colin Smith <colinxsmith@gmail.com>
2025-07-09 22:13:46 +0100

    .

--- getjazz.sh
+++ getjazz.sh
@@ -3,8 +3,9 @@
 #DATE is the start time of the program
 DATE=${1:-20250323}
 TIME=${2:-1700}
+FILE=${3:-new.mp3}
 #both URLs work
 URL1=hellorayo
 URL2=planetradio
-curl -L la.$URL1.co.uk/jazz/Jazz-$DATE-$TIME.mp3 > new.mp3
-id3v2 -c "$(date +%d-%m-%Y) $(uname -m -o -s)" new.mp3
+curl -L la.$URL1.co.uk/jazz/Jazz-$DATE-$TIME.mp3 > $FILE
+id3v2 -c "$(date +%d-%m-%Y) $(uname -m -o -s)" $FILE
