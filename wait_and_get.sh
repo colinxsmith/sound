2a41cb298f99c3621a0a509da466071cd94fd48d
Colin Smith <colinxsmith@gmail.com>
2025-10-27 16:54:54 +0000

    .

--- wait_and_get.sh
+++ wait_and_get.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
-time=${2:-20251005-0600}
+time=${2:-20251005-1700}
 file=${1:-prog.mp3}
+sleeptime=${3:-300}
 attempt=0
 if [ -f $file ]; then rm $file; fi
 touch $file
 while [ $(ls -l $file | awk '{print $5}') == '0' ]; 
 do 
   attempt=$(($attempt + 1))
-  echo Attempt $attempt
+  echo Attempt $attempt $(date)
   echo -e "curl -L https://la.hellorayo.co.uk/jazz/Jazz-$time.mp3 > $file"
   curl -L https://la.hellorayo.co.uk/jazz/Jazz-$time.mp3 > $file;
-  if [ $(ls -l $file | awk '{print $5}') == '0' ]; then sleep 60; fi 
+  if [ $(ls -l $file | awk '{print $5}') == '0' ]; then sleep $sleeptime; fi 
 done
 
 ls -l $file
