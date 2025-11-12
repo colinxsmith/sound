732d8104174c35f8e8ad36f664c5b27aa6ab4274
Colin Smith <colinxsmith@gmail.com>
2025-11-12 21:27:04 +0000

    .

--- wwfmget.sh
+++ wwfmget.sh
@@ -5,6 +5,7 @@ trixie64=trixie64
 if [ $here -eq 0 ]; then if [ $(uname -n) != $trixie64 ]; then export MPD_HOST=$trixie64.local; fi; fi
 track=$(mpc playlist | grep -i "$station")
 track=$(echo $track | awk -F: '{ print $2; }')
+track=$(echo $track | sed "s/^-//")
 echo $track
 
 uname -n
